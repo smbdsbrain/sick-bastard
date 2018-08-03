93f530a514401cbb428d4cb17f44ab474d8facb8
Pavel Kolokdin <pk@napoleonit.ru>
2018-08-03 17:44:09 +0500

    🗃

--- models/base.py
+++ models/base.py
@@ -7,24 +7,27 @@ from sqlalchemy.orm import relationship
 Base = declarative_base()
 
 
-
 class Vertex(Base):
     __tablename__ = 'vertexes'
     id = Column(Integer, primary_key=True)
     word = Column(String, nullable=False)
 
+
 class Edge(Base):
     __tablename__ = 'edges'
     id = Column(Integer, primary_key=True)
-    from_vertex = ForeignKey('vertexes.id', nullable=False)
-    to_vertex = ForeignKey('vertexes.id', nullable=False)
+    from_vertex = Column(Integer, nullable=False)
+    to_vertex = Column(Integer, nullable=False)
+    count = Column(Integer, nullable=False, default=0)
+
 
 class Begin(Base):
     __tablename__ = 'begins'
     id = Column(Integer, primary_key=True)
-    vertex_id = ForeignKey('vertexes.id', nullable=False)
+    vertex_id = Column(Integer,  nullable=False)
+
 
 class End(Base):
     __tablename__ = 'ends'
     id = Column(Integer, primary_key=True)
-    vertex_id = ForeignKey('vertexes.id', nullable=False)
+    vertex_id = Column(Integer,  nullable=False)
