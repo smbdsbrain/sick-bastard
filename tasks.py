fc05477698fca112f523ad1a9dfa7be3dd2f4837
Pavel Kolokdin <pk@napoleonit.ru>
2018-08-03 17:44:25 +0500

    ♻️

--- tasks.py
+++ tasks.py
@@ -1,9 +1,12 @@
 import logging
+import pathlib
 
-import asyncio
 from invoke import task
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 logging.basicConfig(level=logging.INFO)
+Session = sessionmaker(autocommit=True)
 
 
 @task(default=True)
@@ -13,7 +16,23 @@ def run(ctx):
     """
 
     from sick_bastard.chain import add_corpus
-    with open('corpuses/1') as f:
-        corpus = f.read()
 
-    add_corpus(corpus)
+    ctx.config.engine = engine = create_engine(ctx.config.postgres)
+    ctx.config.connection = engine.connect()
+    Session.configure(bind=engine)
+
+    work_dir = pathlib.Path(__file__).parent / 'corpuses'
+    for file in work_dir.iterdir():
+        corpus = file.read_text(encoding='utf-8')
+        add_corpus(ctx.config, corpus)
+
+
+@task
+def build_phrase(ctx):
+    from sick_bastard.chain import build_phrase
+
+    ctx.config.engine = engine = create_engine(ctx.config.postgres)
+    ctx.config.connection = engine.connect()
+    Session.configure(bind=engine)
+
+    build_phrase(ctx.config)
